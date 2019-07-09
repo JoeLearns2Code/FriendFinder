450e776660131f4c17604168a7b43f373f7b60f4
Joe Hutchinson <48301850+JoeLearns2Code@users.noreply.github.com>
2019-07-09 01:15:58 -0700

    More work on js.

--- server.js
+++ server.js
@@ -7,10 +7,20 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
+// Set up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Router - point the server to the route js files
+
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
 app.get('/', function(req,res){
     res.send('hello joe')
 });
 
+//Listener
 app.listen(PORT, function(){
     console.log('listening on ' + PORT)
 });
