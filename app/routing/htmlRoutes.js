f265dfd785febe28f723623769cbf2b13e17a8fb
Joe Hutchinson <48301850+JoeLearns2Code@users.noreply.github.com>
2019-07-08 19:47:11 -0700

    Created HTML files; added routing

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,17 @@
+//Initialize path module
+var path = require("path");
+
+//Export HTML
+
+module.exports = function (app) {
+    //Home Page
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/home.html'))
+    });
+
+    //Survey Page
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"))
+    });
+
+};
