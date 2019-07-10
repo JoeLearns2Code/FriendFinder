4be4ee225ac6f994e88799904927f12759b26a23
Joe Hutchinson <48301850+JoeLearns2Code@users.noreply.github.com>
2019-07-09 22:11:25 -0700

    Adding JS for matchmaking.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,6 +15,33 @@ module.exports = function(app) {
     app.post("/api/friends", function(req,res){
         var input = req.body;
         console.log("Client input: " + JSON.stringify(input));
-    });
 
+        //variable for user answers, based on scores array
+        var responses = userData.scores;
+
+        //variables for best match
+        var matchName = "";
+        var matchImage = "";
+        var totalDifference = 100;  //Initial value larger than any possible difference comparison
+
+        //Check all existing friends in the object
+        for (var i = 0; i < friends.length; i++) {
+
+            //calculate difference for each question
+            var difference = 0;
+            for (var j = 0; j < responses.length; j++) {
+                difference += Math.abs(friends[i].scores[j] - responses[j]);
+            }
+            //match friend if the difference is the least
+            if (difference < totalDifference) {
+                totalDifference = difference
+                matchName = friends[i].name;
+                matchImage = friends[i].photo;
+            }
+        }
+        //Adding the new user to the friends data
+        friends.push(input);
+        //Send response.json
+        res.json({status: "OK", matchName: matchName, matchImage: matchImage});
+    });
 };
